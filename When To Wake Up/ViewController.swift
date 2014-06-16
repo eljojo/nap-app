9cd55cbcaecaa058d315dda1a0731aa21d5f5969
José Tomás Albornoz <jojo@eljojo.net>
2014-06-16 19:19:05 +0200

    Initial Commit

---
+++ When To Wake Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  When To Wake Up
+//
+//  Created by José Albornoz on 16/06/14.
+//  Copyright (c) 2014 José Albornoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ When To Wake UpTests/When_To_Wake_UpTests.swift
@@ -0,0 +1,35 @@
+//
+//  When_To_Wake_UpTests.swift
+//  When To Wake UpTests
+//
+//  Created by José Albornoz on 16/06/14.
+//  Copyright (c) 2014 José Albornoz. All rights reserved.
+//
+
+import XCTest
+
+class When_To_Wake_UpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
