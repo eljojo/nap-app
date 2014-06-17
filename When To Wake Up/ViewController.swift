077f98a67c7124f2705a62b2bbfe0575c6596756
José Tomás Albornoz <jojo@eljojo.net>
2014-06-17 13:23:36 +0200

    added nap times

--- When To Wake Up/ViewController.swift
+++ When To Wake Up/ViewController.swift
@@ -20,15 +20,18 @@ class ViewController: UIViewController {
     @IBOutlet var eightTime:  UILabel!
     @IBOutlet var fallAsleepSwitch: UISwitch!
     
-    var timeLabels = UILabel[]()
+    var sleepLabels = UILabel[]()
+    var napLabels   = UILabel[]()
     let sleepCycle: Double = 5400
+    let napTime: Double = 20 * 60
     let timeToFallAsleep: Double = 14 * 60
-
 
     let gray        = UIColor(red: 102.0/255.0, green: 102.0/255.0, blue: 102.0/255.0, alpha: 1.0)
     let lightGreen  = UIColor(red: 153.0/255.0, green: 204.0/255.0, blue: 102.0/255.0, alpha: 1.0)
     let darkGreen   = UIColor(red: 0.0/255.0,   green: 204.0/255.0, blue: 51.0/255.0,  alpha: 1.0)
-    var colours     = UIColor[]()
+    let orange      = UIColor(red: 254.0/255.0, green: 102.0/255.0, blue: 2.0/255.0,   alpha: 1.0)
+    var sleepColours = UIColor[]()
+    var napColours   = UIColor[]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,28 +46,44 @@ class ViewController: UIViewController {
     //Returning to view
     override func viewWillAppear(animated: Bool) {
         // this code is pretty horrible
-        if timeLabels.count == 0 {
-            timeLabels = [firstTime, secondTime, thirdTime, fourthTime, fifthTime, sixthTime, seventhTime, eightTime]
+        if sleepLabels.count == 0 {
+            sleepLabels = [secondTime, thirdTime, fourthTime, fifthTime, sixthTime, seventhTime, eightTime]
+        }
+        if napLabels.count == 0 {
+            napLabels = [firstTime]
         }
 
         // i have no freaking clue on how to do class variables in swift
-        if colours.count == 0 {
-            colours = [gray, gray, gray, lightGreen, darkGreen, darkGreen, darkGreen, darkGreen]
+        if sleepColours.count == 0 {
+            sleepColours = [gray, gray, gray, lightGreen, darkGreen, darkGreen, darkGreen]
+        }
+        if napColours.count == 0 {
+            napColours = [orange]
         }
         
         setTimes()
     }
 
     func setTimes() {
-        var date = NSDate()
+        var now = NSDate()
+        var dateForSleep = now
+
         if fallAsleepSwitch.on == true {
-            date = date.dateByAddingTimeInterval(timeToFallAsleep)
+            dateForSleep = dateForSleep.dateByAddingTimeInterval(timeToFallAsleep)
         }
-        for (index, timeLabel) in enumerate(timeLabels) {
-            var extraTime: NSTimeInterval = sleepCycle * Double(index + 1)
-            var newDate = date.dateByAddingTimeInterval(extraTime)
-            timeLabel.text = formatTime(newDate)
-            timeLabel.textColor = colours[index]
+
+        for (index, timeLabel) in enumerate(sleepLabels) {
+            let extraTime: NSTimeInterval = sleepCycle * Double(index + 1)
+            let sleepTime = dateForSleep.dateByAddingTimeInterval(extraTime)
+            timeLabel.text = formatTime(sleepTime)
+            timeLabel.textColor = sleepColours[index]
+        }
+
+        for (index, timeLabel) in enumerate(napLabels) {
+            let extraTime: NSTimeInterval = napTime * Double(index + 1)
+            let sleepTime = now.dateByAddingTimeInterval(extraTime)
+            timeLabel.text = formatTime(sleepTime)
+            timeLabel.textColor = napColours[index]
         }
     }
 
